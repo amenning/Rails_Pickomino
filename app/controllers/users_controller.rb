1814c527605382fc14849507ca4cf68ad80e7ebd
metakata <menning23@gmail.com>
2016-04-02 10:54:11 -0400

    add nil location to all Application Controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
   def create
-    respond_with User.create(user_params)
+    respond_with User.create(user_params), location: nil
   end
   
   def show
-    respond_with User.find(params[:username, :password])
+    respond_with User.find(params[:username, :password]), location: nil
   end
   
   private
